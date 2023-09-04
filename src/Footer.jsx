2cf08c22adea273c0b74c9b99cb48541c2622057
Sophia WM <claudiasophiawong@gmail.com>
2023-09-04 21:57:40 +0200

    feature: start-footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import 'footer.css';
+export default function Footer() {
+    return (
+      <>
+        <footer className="footer-general-container">
+          <img className="footer-logo" src="assets/logo-berry-good.png" alt="Berry Goods Logo"></img>
+          <h2 className="footer-calltoaction">Sell and buy delicious designs!</h2>
+        </footer>
+      </>
+    );
+  }
+  
