54b9112d4fab691c5f03905dc2512ccd63e75dcc
Ruben Reyes <rubenreyes4v@gmail.com>
2023-09-06 10:08:47 +0200

    feature-shopdata/finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import InfoComponent from './InfoComponent'; 
+import InfoComponent from './components/InfoComponent'; 
 
 function App() {
   return (
---
+++ src/styles/styles.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const ContainerDataShop = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #6134C2;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+export const Section = styled.div`
+  text-align: center;
+  margin: 10px 0;
+`;
+
+export const H4 = styled.h4`
+  color: #F3B605;
+  margin: 0;
+`;
+
+export const P = styled.p`
+  margin: 0;
+  color: #F3B605;
+`;
+
+export const Button = styled.button`
+  background-color: #F3B605;
+  color: #6134C2;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 20px;
+  cursor: pointer;
+  margin-bottom: 2rem;
+`;
